ae0f66be92bc2abdd13d8f94a95148234fe81888
livio <livio.personne@gmail.com>
2026-06-23 20:02:41 +0200

    cardiac beat with noise

--- tp-ras-pico/src/visu.py
+++ tp-ras-pico/src/visu.py
@@ -1,21 +1,32 @@
 import matplotlib.pyplot as plt
 import os
+import random
 
 folder = os.path.join(os.getcwd(), "data")
 filename = "cardiac.txt"
 file = open(os.path.join(folder, filename), 'r')
+Y = list(map(lambda x: float(x.strip()), file.readlines()))
+data = enumerate(Y)
+n = len(Y)
+X = range(0, n)
 
-def show_beat():
-    Y = list(map(lambda x: float(x.strip()), file.readlines()))
-    X = range(0, len(Y))
+def add_noise(intensity=0.1):
+    for i in range(n):
+        Y[i] += random.normalvariate(0, intensity)
 
-    print(X[:5], Y[:5])
 
-    plt.plot(X, Y)
-    plt.grid()
-    plt.show()
+def naive1():
+    window_size = 300
+    max_relative = 0.5 # Max accepted relative difference between peak intensities
+    local_maxes = []
+    for i in range(1, n-1):
+        if (Y[i] >= Y[i-1] and Y[i] >= Y[i+1]):
+            local_maxes.append((i, Y[i]))
 
 
 if __name__ == '__main__':
-    show_beat()
+    # add_noise(0.3)
+    plt.plot(X, Y)
+    plt.grid()
+    plt.show()
     file.close()
